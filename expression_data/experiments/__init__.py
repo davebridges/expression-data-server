384d48d4516a0dc21dff3ea1d02e76d3bc1fea17
Dave Bridges <dave.bridges@gmail.com>
2012-10-13 21:02:27 -0400

    Wrote docstring for experiment app.

---
+++ expression_data/experiments/__init__.py
@@ -0,0 +1,8 @@
+'''The experiments app describes various types of gene expression experiments.
+
+The goals of this app are to:
+
+* Link experimental data (the :mod:`data` app) with genes (the :mod:`genes` app).
+* Centralize and rationalize metadata for an experiment such as analysis conditions, researchers and samples.
+* Provide a convenient way to list all inputted data.
+'''
