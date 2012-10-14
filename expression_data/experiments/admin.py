55248f06613efed23dff96bf8100524355d7b654
Dave Bridges <dave.bridges@gmail.com>
2012-10-13 21:54:53 -0400

    Added generic admin interfaces for experiments app.  Part of issue #4.

---
+++ expression_data/experiments/admin.py
@@ -0,0 +1,41 @@
+'''
+This package has the admin interface for the :mod:`experiments` app.
+
+All objects have a generic Admin interface.
+'''
+
+from django.contrib import admin
+from experiments.models import mRNASeqExperiment, SequenceAlignmentSoftware, DifferentialExpressionSoftware, ReferenceGenomeAssembly, Sample, Manipulation
+
+class mRNASeqExperimentAdmin(admin.ModelAdmin):
+    '''Generic admin interface for :class:`~experiments.models.mRNASeqExperiment' objects.'''
+    pass
+admin.site.register(mRNASeqExperiment, mRNASeqExperimentAdmin)
+
+class SequenceAlignmentSoftwareAdmin(admin.ModelAdmin):
+    '''Generic admin interface for :class:`~experiments.models.SequenceAlignmentSoftware' objects.'''
+    pass
+admin.site.register(SequenceAlignmentSoftware, SequenceAlignmentSoftwareAdmin)
+
+class DifferentialExpressionSoftwareAdmin(admin.ModelAdmin):
+    '''Generic admin interface for :class:`~experiments.models.DifferentialExpressionSoftware' objects.'''
+    pass
+admin.site.register(DifferentialExpressionSoftware, DifferentialExpressionSoftwareAdmin)
+
+class ReferenceGenomeAssemblyAdmin(admin.ModelAdmin):
+    '''Generic admin interface for :class:`~experiments.models.ReferenceGenomeAssembly' objects.'''
+    pass
+admin.site.register(ReferenceGenomeAssembly, ReferenceGenomeAssemblyAdmin)
+
+class SampleAdmin(admin.ModelAdmin):
+    '''Generic admin interface for :class:`~experiments.models.Sample' objects.'''
+    pass
+admin.site.register(Sample, SampleAdmin)
+
+class ManipulationAdmin(admin.ModelAdmin):
+    '''Generic admin interface for :class:`~experiments.models.Manipulation' objects.'''
+    pass
+admin.site.register(Manipulation, ManipulationAdmin)
+
+
+    
