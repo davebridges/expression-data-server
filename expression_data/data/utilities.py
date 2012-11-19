dc3412785aa8b5f79502f9ec74b947b2d907794a
Dave Bridges <dave.bridges@gmail.com>
2012-11-18 19:14:17 -0500

    Wrote a basic function to parse gene_exp.diff files.  Part of issue #5 and #13.

---
+++ expression_data/data/utilities.py
@@ -0,0 +1,61 @@
+'''This package contains utility functions for the :mod:`data` app.
+
+These functions import data from a variety of tables into the database.
+'''
+
+import csv
+
+from data.models import GeneExperimentData
+from experiments.models import mRNASeqExperiment
+from genes.models import Gene
+
+def cufflinks_gene_diff_import(experiment_id):
+    '''This function imports the data from a gene_exp.diff into :class:`~data.models.GeneExperimentData` objects.
+    
+    This function works manually by defining the file location in the filename line.
+    '''
+    
+    filename = "/Users/davebrid/Documents/SRC/expression-data-server/expression_data/data/fixtures/sample_gene_exp.diff"
+    print "Updating from file %s" % filename
+    experiment = mRNASeqExperiment.objects.get(pk=experiment_id)
+    with open(filename, 'r') as inputfile:
+        for row in csv.DictReader(inputfile, delimiter='\t'):
+            try: 
+                datum = GeneExperimentData(
+                     experiment=experiment,
+        			 gene=Gene.objects.get(pk=row['gene']),
+        			 fold_change = row['log2(fold_change)'],
+        			 p_value = row['p_value'],
+        			 q_value = row['q_value'],
+        			 locus = row['locus'],
+        			 internal_id = row['test_id'],
+        			 sample_1 = row['sample_1'],
+        			 sample_2 = row['sample_2'],
+        			 amount_1 = row['value_1'],
+        			 amount_2 = row['value_2'],
+        			 status = row['status'],
+        			 test_statistic = row['test_stat'],
+        			 significant = row['significant'])
+                datum.save()
+                print "Updated %s" % row['gene']
+            except Gene.DoesNotExist:
+                new_gene = Gene(name=row['gene'])
+                new_gene.save()
+                datum = GeneExperimentData(
+                     experiment=experiment,
+        			 gene=new_gene,
+        			 fold_change = row['log2(fold_change)'],
+        			 p_value = row['p_value'],
+        			 q_value = row['q_value'],
+        			 locus = row['locus'],
+        			 internal_id = row['test_id'],
+        			 sample_1 = row['sample_1'],
+        			 sample_2 = row['sample_2'],
+        			 amount_1 = row['value_1'],
+        			 amount_2 = row['value_2'],
+        			 status = row['status'],
+        			 test_statistic = row['test_stat'],
+        			 significant = row['significant'])
+                datum.save()
+                print "Created New Gene Named %s" % row['gene']                
+                
